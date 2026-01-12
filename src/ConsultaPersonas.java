1018f769d0357140cff1702355c36d53400c30dd
gtapiero <giovanny.tapiero@pi.edu.co>
2026-01-12 18:35:23 -0500

    commit - ConsultaPersonas .

--- src/ConsultaPersonas.java
+++ src/ConsultaPersonas.java
@@ -1,6 +1,12 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class ConsultaPersonas extends JFrame {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
